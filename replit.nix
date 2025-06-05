653b15019ee0eefbc9d579cf60922e905feb85c6
Blumist Kaagii <43357562-mariunbold226@users.noreply.replit.com>
2025-06-05 17:48:53 +0000

    Post-Recovery checkpoint

--- replit.nix
+++
@@ -1,8 +0,0 @@
-{ pkgs }: {
-  deps = [
-    pkgs.python311
-    pkgs.python311Packages.firebase-admin
-    pkgs.python311Packages.python-dotenv
-    pkgs.python311Packages.python-telegram-bot
-  ];
-}

b1129e37103b9f00c5d2cff840832f4f1a2f7e50
Blumist Kaagii <43357562-mariunbold226@users.noreply.replit.com>
2025-06-14 10:11:10 +0000

    Pre-Recovery checkpoint

--- replit.nix
+++ replit.nix
@@ -2,6 +2,9 @@
   deps = [
     pkgs.python311
     pkgs.python311Packages.pip
+    pkgs.python311Packages.requests
+    pkgs.python311Packages.firebase-admin
+    pkgs.python311Packages.python-telegram-bot
     pkgs.python311Packages.setuptools
     pkgs.python311Packages.wheel
   ];
